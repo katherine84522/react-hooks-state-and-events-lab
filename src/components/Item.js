395d5c07f1069abf61779220b07a594972e25dd5
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-11-30 21:37:59 -0500

    commit tree 37c70680e56cd49640132bf8754598dd5a95c0e4

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 
 function Item({ name, category }) {
+
+  const [cart, setCart] = useState(false)
+
+  const addToCart = () => {
+    setCart(!cart)
+  }
+
+  const appClass = cart ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={() => { addToCart() }} className="add"> {cart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
